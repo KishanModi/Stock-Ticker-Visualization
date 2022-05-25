ffbc401b061956a03739fd1e9660e39cfda78b47
KishanModi <kishandmodi@gmail.com>
2022-05-25 14:26:14 +0530

    few visual changes

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import datetime as dt
 import time
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-
-
+import plotly.io as pio
+pio.templates.default = "plotly_white"
 data = pd.read_csv('final.csv')
 
 
@@ -19,30 +19,66 @@ Candle = go.Candlestick(x=data['Date'],
                 open=data['Open'],
                 high=data['High'],
                 low=data['Low'],
-                close=data['Close'])
+                close=data['Close'],legendgroup="Price",name="Price")
 ATR = go.Scatter(x=data['Date'],
-                y=data['atr'])
+                y=data['atr'],legendgroup="ATR",name="ATR")
 
 ATR_STD = go.Scatter(x=data['Date'],
-                y=data['atr_std'])
+                y=data['atr_std'],legendgroup="ATR_STD",name="ATR_STD")
 ATR_CLOSE = go.Scatter(x=data['Date'],
-                y=data['close_std'])
+                y=data['close_std'],legendgroup="ATR_CLOSE",name="ATR_CLOSE")
 
-fig = make_subplots(rows=4, cols=1,
+fig = make_subplots(rows=5, cols=1,
                           shared_xaxes=True, shared_yaxes=False,
-                          vertical_spacing=0.1,row_width=[3,3,0.5,14])
+                           vertical_spacing=0.095,row_width=[2,2,2,0.5,14],subplot_titles=("Price", "","ATR", "ATR_STD", "ATR_CLOSE"))
 fig.append_trace(Candle, row=1, col=1)
 
 fig.append_trace(ATR,row=3, col=1)
 
 fig.append_trace(ATR_STD,row=4, col=1)
 
-fig.append_trace(ATR_CLOSE,row=4, col=1)
+fig.append_trace(ATR_CLOSE,row=5, col=1)
 
 
-fig.update_layout(height=700, width=1000,
-                  title='Data Visulization',
-                  title_text="Multiple Subplots with Titles")
-fig.layout.yaxis2.showgrid=False
+fig.update_layout(title_text="Data Visualization",
+                  height=700, width=1000)
+# fig.update_xaxes(
+#     rangeselector=dict(
+#         buttons=list([
+#             dict(count=1, label="1m", step="month", stepmode="backward"),
+#             dict(count=6, label="6m", step="month", stepmode="backward"),
+#             dict(count=1, label="YTD", step="year", stepmode="todate"),
+#             dict(count=1, label="1y", step="year", stepmode="backward"),
+#             dict(step="all")
+#         ])
+#     ),
+#     xaxis2_rangeslider_visible=True,
+#     xaxis2_type="date"
+# )
+fig.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=[
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=6,
+                     label="6m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="YTD",
+                     step="year",
+                     stepmode="todate"),
+                dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                dict(step="all")
+            ]),
+        type="date")
+    )
+
 with st.container():
 	st.plotly_chart(fig,use_container_width=True)
