aec81a1ac0fe461b56e0a3eed9801cc33831b585
KishanModi <kishandmodi@gmail.com>
2022-05-25 16:53:44 +0530

    visual changes

--- app.py
+++ app.py
@@ -1,84 +1,27 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import datetime as dt
 import time
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import plotly.io as pio
-pio.templates.default = "plotly_white"
+st.set_page_config(layout="wide")
+from calc import *
+
 data = pd.read_csv('final.csv')
 
 
-st.set_page_config(layout="wide")
+st.title('German 10 YR Bund Futures - Jun 22')
 
-## Main Title ##
-st.title('Data Visualization')
-
-Candle = go.Candlestick(x=data['Date'],
-                open=data['Open'],
-                high=data['High'],
-                low=data['Low'],
-                close=data['Close'],legendgroup="Price",name="Price")
-ATR = go.Scatter(x=data['Date'],
-                y=data['atr'],legendgroup="ATR",name="ATR")
-
-ATR_STD = go.Scatter(x=data['Date'],
-                y=data['atr_std'],legendgroup="ATR_STD",name="ATR_STD")
-ATR_CLOSE = go.Scatter(x=data['Date'],
-                y=data['close_std'],legendgroup="CLOSE_STD",name="CLOSE_STD")
-
-fig = make_subplots(rows=5, cols=1,
-                          shared_xaxes=True, shared_yaxes=False,
-                           vertical_spacing=0.075,row_width=[3,3,3,0.1,10],subplot_titles=("Price", "","ATR", "ATR_STD", "CLOSE_STD"))
-fig.append_trace(Candle, row=1, col=1)
-
-fig.append_trace(ATR,row=3, col=1)
-
-fig.append_trace(ATR_STD,row=4, col=1)
-
-fig.append_trace(ATR_CLOSE,row=5, col=1)
+#slider for MSD window
+st.subheader('Enter The Moving Standard Deviation Window:')
+MSDwindow = st.slider('Range', 2, 30, 14)
 
 
-fig.update_layout(title_text="Data Visualization",
-                  height=900, width=1400)
-# fig.update_xaxes(
-#     rangeselector=dict(
-#         buttons=list([
-#             dict(count=1, label="1m", step="month", stepmode="backward"),
-#             dict(count=6, label="6m", step="month", stepmode="backward"),
-#             dict(count=1, label="YTD", step="year", stepmode="todate"),
-#             dict(count=1, label="1y", step="year", stepmode="backward"),
-#             dict(step="all")
-#         ])
-#     ),
-#     xaxis2_rangeslider_visible=True,
-#     xaxis2_type="date"
-# )
-fig.update_layout(
-    xaxis=dict(
-        rangeselector=dict(
-            buttons=[
-                dict(count=1,
-                     label="1m",
-                     step="month",
-                     stepmode="backward"),
-                dict(count=6,
-                     label="6m",
-                     step="month",
-                     stepmode="backward"),
-                dict(count=1,
-                     label="YTD",
-                     step="year",
-                     stepmode="todate"),
-                dict(count=1,
-                     label="1y",
-                     step="year",
-                     stepmode="backward"),
-                dict(step="all")
-            ]),
-        type="date",rangeslider_thickness = 0.05)
-    )
+#submit button
+if st.button('Visulize'):
+    with st.spinner('Wait for it...'):
+        time.sleep(1)
+        fig,close,change, date = execute(data, MSDwindow)
 
-with st.container():
-	st.plotly_chart(fig,use_container_width=True)
+    col1, col2 = st.columns(2)
+    col1.metric("Current Price",close, change)
+    col2.metric("Date", date)
+
+    with st.container():
+	    st.plotly_chart(fig,use_container_width=True)
---
+++ calc.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+data = pd.read_csv("data1.csv")
+
+#calculating MSD value for userdefined window
+def MSD(data, n):
+    data["close_std"] = data["Close"].rolling(n).std()
+    data["atr_std"] = data["atr"].rolling(n).std()
+    return data
+
+def plots(data):
+
+     #candle stick graph for bonds future
+    Candle = go.Candlestick(
+        x=data["Date"],
+        open=data["Open"],
+        high=data["High"],
+        low=data["Low"],
+        close=data["Close"],
+        legendgroup="Price",
+        name="Price",
+    )
+    #Average True Range Graph
+    ATR = go.Scatter(
+        x=data["Date"], y=data["atr"], legendgroup="ATR", name="Average True Range"
+    )
+    #Moving Standard Deviation graph
+    ATR_STD = go.Scatter(
+        x=data["Date"], y=data["atr_std"], legendgroup="ATR_STD", name="ATR MSD"
+    )
+    ATR_CLOSE = go.Scatter(
+        x=data["Date"],
+        y=data["close_std"],
+        legendgroup="CLOSE_STD",
+        name="CLOSE Price MSD",
+    )
+
+    fig = make_subplots(
+        rows=5,
+        cols=1,
+        shared_xaxes=True,
+        shared_yaxes=False,
+        vertical_spacing=0.075,
+        row_width=[3, 3, 3, 0.1, 10],
+        subplot_titles=(
+            "Bond Price Over the time",
+            "",
+            "Average True Range",
+            "ATR Moving Standard Deviation",
+            "CLOSE Price Moving Standard Deviation",
+        ),
+    )
+    fig.append_trace(Candle, row=1, col=1)
+
+    fig.append_trace(ATR, row=3, col=1)
+
+    fig.append_trace(ATR_STD, row=4, col=1)
+
+    fig.append_trace(ATR_CLOSE, row=5, col=1)
+
+    fig.update_layout(
+        title_text="German 10 YR Bund Futures - Jun 22", height=900, width=1400
+    )
+
+    #adding time frame buttons
+    fig.update_layout(
+        xaxis=dict(
+            rangeselector=dict(
+                buttons=[
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(count=1, label="YTD", step="year", stepmode="todate"),
+                    dict(count=1, label="1y", step="year", stepmode="backward"),
+                    dict(step="all"),
+                ]
+            ),
+            type="date",
+            rangeslider_thickness=0.05,
+        )
+    )
+    #axis lines
+    fig.update_xaxes(showline=True, linewidth=2, linecolor="black", mirror=True)
+    fig.update_yaxes(showline=True, linewidth=2, linecolor="black", mirror=True)
+    fig.update_xaxes(showgrid=True)
+    fig.update_yaxes(showgrid=True)
+    return fig
+
+
+def execute(data, n):
+    data = MSD(data, n)
+    fig = plots(data)
+    close = data["Close"].iloc[-1]
+    change = float(data["Change %"].iloc[-1][:-1])
+    date = data["Date"].iloc[-1]
+    return fig, close, change, date
